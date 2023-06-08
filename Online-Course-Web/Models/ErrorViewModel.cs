d2d1deac11fa6ca4286d1e3c628770bf10ae2c36
Sulav-Don631 <Sulavshrestha631@gmail.com>
2023-06-08 08:55:30 +0545

    Initial Web

---
+++ Online-Course-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Online_Course_Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
